857c4547e07efb3c79695e6b700e009939c3ad36
ab0904076 <55218955+ab0904076@users.noreply.github.com>
2019-09-13 19:04:12 -0500

    Add Files Via Upload
    Uploaded the most current version of the project onto GitHub

---
+++ scripts.js
@@ -0,0 +1,5 @@
+var greetings = "Hello World!"
+var message = "<p>greetings" + " is the phrase often used by web developers in simple websites and programs.""</p>";
+function helloWorld(greetings){
+  document.getElementById('functionOne').innerHTML = message;
+}
